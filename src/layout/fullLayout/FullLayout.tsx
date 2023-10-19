eee0977714a241951c184e3ea513bc982df27c6a
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-10-19 12:10:24 +0200

    working

--- src/layout/fullLayout/FullLayout.tsx
+++ src/layout/fullLayout/FullLayout.tsx
@@ -1,16 +1,29 @@
 import React from 'react'
-import { CenteredContent } from "@dhis2/ui";
 import style from "../Layout.module.css"
 import { MainHeader, SideBar } from '../../components'
 import { useGetInitialValues } from '../../hooks/initialValues/useGetInitialValues'
+import { CenteredContent, CircularLoader } from "@dhis2/ui";
+import { useGetProgramConfig } from '../../hooks/programConfig/useGetprogramConfig';
+import { getSelectedKey } from '../../utils/commons/dataStore/getSelectedKey';
 
 export default function FullLayout({ children }: { children: React.ReactNode }) {
+    useGetInitialValues()
     const { isSetSectionType } = useGetInitialValues()
+    const { getDataStoreData } = getSelectedKey()
+    const { loading } = useGetProgramConfig(getDataStoreData.program);
 
     if (!isSetSectionType) {
         return (
             <CenteredContent>
                 Cant load the app without section type
+            </CenteredContent>
+        )
+    }
+
+    if (loading) {
+        return (
+            <CenteredContent>
+                <CircularLoader />
             </CenteredContent>
         )
     }
