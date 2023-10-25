8f72f7d349bf4fa82884d5ca397c023dd29e7ba5
edsonhancale <110174899+edsonhancale@users.noreply.github.com>
2023-10-25 15:22:34 +0200

    feat: fixed bug in sideBar data

--- src/hooks/dataElements/useDataElementsParamMapping.ts
+++ src/hooks/dataElements/useDataElementsParamMapping.ts
@@ -3,9 +3,9 @@ import { getSelectedKey } from "../../utils/commons/dataStore/getSelectedKey"
 const useDataElementsParamMapping = () => {
     const { getDataStoreData } = getSelectedKey()
     return {
-        [getDataStoreData.registration.employmentType]: "employmentType",
-        [getDataStoreData.registration.academicYear]: "academicYear",
-        [getDataStoreData.registration.position]: "position"
+        [getDataStoreData?.registration?.employmentType]: "employmentType",
+        [getDataStoreData?.registration?.academicYear]: "academicYear",
+        [getDataStoreData?.registration?.position]: "position"
     }
 }
 export default useDataElementsParamMapping;
