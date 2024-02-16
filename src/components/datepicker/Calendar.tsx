ee806d51e54134fd80424fa22b2a05307bb36f51
EdsonNhancale <edsonnhancale97@gmail.com>
2024-02-16 16:20:38 +0200

    [EMISWEB-103] added function to disable unavalible schools days

--- src/components/datepicker/Calendar.tsx
+++ src/components/datepicker/Calendar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers';
+import { unavailableSchoolDays } from '../../utils/constants/attendance/unavailableSchoolDays';
 
 interface CalendarProps {
   value: { selectedDate: Date }
@@ -10,12 +11,14 @@ interface CalendarProps {
 
 export default function Calendar(props: CalendarProps) {
   const { value, setValue } = props
+  const { unavailableDays } = unavailableSchoolDays()
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DateCalendar
         value={value.selectedDate}
         onChange={(e) => { setValue({ selectedDate: e as Date }) }}
+        shouldDisableDate={(date) => unavailableDays(date)}
       />
     </LocalizationProvider>
   );
