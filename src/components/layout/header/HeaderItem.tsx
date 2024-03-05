8afa69d1239cab3a78aa2275c2e77f4d9200194a
cnandza <clayd.nandza@saudigitus.org>
2024-03-05 11:27:23 +0200

    EMISWEB-109 Feat: Implement global filters reset

--- src/components/layout/header/HeaderItem.tsx
+++ src/components/layout/header/HeaderItem.tsx
@@ -6,12 +6,26 @@ import { DropdownButton, FlyoutMenu } from "@dhis2/ui"
 import info from "../../../assets/images/headbar/info.svg"
 import { HeadBarTypes } from '../../../types/headBar/HeadBarTypes'
 import { componentMapping } from '../../../utils/commons/componentMapping'
+import { useDataElementsParamMapping, useParams } from '../../../hooks'
+import HeaderResetItemValue from './HeaderResetItemValue'
 
 export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
-    const { label, value, placeholder, component, dataElementId, id } = props;
-    const Component = (component != null) ? componentMapping[component] : null;
+    const { label, value, placeholder, component, dataElementId, id, selected } = props;
+    const { remove } = useParams()
+    const paramsMapping = useDataElementsParamMapping()
     const [openDropDown, setOpenDropDown] = useState<boolean>(false);
     const onToggle = () => { setOpenDropDown(!openDropDown) }
+    const Component = (component != null) ? componentMapping[component] : null;
+
+    const onReset = () => {
+        if(dataElementId)
+            remove(paramsMapping[dataElementId as unknown as keyof typeof paramsMapping])
+        else
+            if(id === "c540ac7c") {
+                remove("school");
+                remove("schoolName");
+            }
+    }
 
     return (
         <DropdownButton
@@ -27,6 +41,7 @@ export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
             }
         >
             <h5>{label} <span>{value}</span></h5>
+            {selected && <HeaderResetItemValue onReset={onReset}/> }
             <img src={info} />
         </DropdownButton >
     )
