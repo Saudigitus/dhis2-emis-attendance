04c062ec2da842d8a5b4a928b5e2876c60bc5ec8
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-05 16:13:25 +0200

    [EMISWEB-130] enhancement: added table title

--- src/components/table/render/Table.tsx
+++ src/components/table/render/Table.tsx
@@ -15,6 +15,17 @@ import {useHeader, useTableData, useParams, useAttendanceMode} from '../../../ho
 const usetStyles = makeStyles({
     tableContainer: {
         overflowX: 'auto'
+    },
+    workingListsContainer: {
+        display: 'flex',
+        marginLeft: '0.5rem',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    h4: {
+        margin: '0px',
+        fontSize: '22px',
+        fontWeigth: '500',
     }
 });
 
@@ -69,7 +80,10 @@ function Table() {
                     <CircularLoader/>
                 </CenteredContent>
             }
-            <WorkingLits/>
+            <div className={classes.workingListsContainer}>
+                <h4 className={classes.h4}>Attendance</h4>
+                <WorkingLits/>
+            </div>
             <WithBorder type='bottom'/>
             <WithPadding>
                 <WithBorder type='all'>
