96cd5cd1611bf5f53e437b2f79db50e99fe268d1
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-24 21:52:16 +0200

    resolving mereg bugs

--- src/components/table/render/Table.tsx
+++ src/components/table/render/Table.tsx
@@ -1,17 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import RenderRows from './RenderRows'
 import RenderHeader from './RenderHeader'
-import { Paper } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { CenteredContent, CircularLoader } from "@dhis2/ui";
-import { TeiRefetch } from '../../../schema/refecthTeiSchema';
-import { WithBorder, WithPadding } from '../../../components';
-import { HeaderFieldsState } from '../../../schema/headersSchema';
-import { SelectedDateState } from '../../../schema/attendanceSchema';
-import { HeaderFilters, Pagination, TableComponent, WorkingLists } from '../components'
-import { useHeader, useTableData, useParams, useAttendanceMode } from '../../../hooks';
-import { TableDataLoadingState } from '../../../schema/tableDataLoadingSchema';
+import {Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {CenteredContent, CircularLoader} from "@dhis2/ui";
+import {TeiRefetch} from '../../../schema/refecthTeiSchema';
+import {WithBorder, WithPadding} from '../../../components';
+import {HeaderFieldsState} from '../../../schema/headersSchema';
+import {SelectedDateState} from '../../../schema/attendanceSchema';
+import {HeaderFilters, Pagination, TableComponent, WorkingLists} from '../components'
+import {useHeader, useTableData, useParams, useAttendanceMode} from '../../../hooks';
 
 const usetStyles = makeStyles({
     tableContainer: {
@@ -45,24 +44,18 @@ function Table() {
     const [page, setpage] = useState(1)
     const [pageSize, setpageSize] = useState(10)
     const [refetch] = useRecoilState(TeiRefetch)
-    const { setInitialAttendanceMode, attendanceMode } = useAttendanceMode()
-    const { urlParamiters } = useParams()
-    const { academicYear } = urlParamiters()
-    const setLoading = useSetRecoilState(TableDataLoadingState)
-
-    useEffect(() => {
-        setLoading(loading)
-    }, [loading])
+    const {attendanceMode} = useAttendanceMode()
+    const {urlParamiters} = useParams()
+    const {academicYear} = urlParamiters()
 
     useEffect(() => {
         if (academicYear) {
             void getData(page, pageSize)
-            // setInitialAttendanceMode()
         }
     }, [headerFieldsState, page, pageSize, refetch])
 
     useEffect(() => {
-        if (academicYear)
+        if (academicYear) {
             void getAttendanceData()
         }
     }, [selectedDateViewMode])
