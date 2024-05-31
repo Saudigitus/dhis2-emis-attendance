ea34a9f190884dda4f3d80236dc45dc4763096b8
AlziraXerinda <alziraxerinda0@gmail.com>
2024-05-31 12:21:23 +0200

    [EMISWEB-148] fix: Filter attendance data by enrollment on first data fetching

--- src/hooks/tableData/useTableData.ts
+++ src/hooks/tableData/useTableData.ts
@@ -125,7 +125,7 @@ export function useTableData() {
                 }
 
                 for (const [index, tei] of localData.entries()) {
-                    const attendanceDetails = attendanceValuesByTei.filter((x) => x.trackedEntity === tei.trackedEntity);
+                    const attendanceDetails = attendanceValuesByTei.filter((x) => x.trackedEntity === tei.trackedEntity).filter((attendance: any) => attendance.enrollment === tei.enrollment);
                     localData[index] = { ...tei, ...attendanceFormater(attendanceDetails, attendanceConfig) };
                 }
 
