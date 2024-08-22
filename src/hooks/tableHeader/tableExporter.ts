43f0ae0e51c8db8d9c50c7afe12634e8f75fb156
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-08-22 16:56:28 +0200

    feat: :fire: added validation for attendance, added proctection to avoid adding new cols and rows and blocked all non atteandance cells

--- src/hooks/tableHeader/tableExporter.ts
+++ src/hooks/tableHeader/tableExporter.ts
@@ -1,11 +1,12 @@
 import Excel from 'exceljs'
 import { saveAs } from 'file-saver'
 import { getWorkSheets } from '../../utils/exporter/getWorkSheet';
-import { alinhamento, border, fill } from '../../utils/exporter/exporterConsts';
+import { alinhamento, border, dataValidation, fill, lock } from '../../utils/exporter/exporterConsts';
 
 export async function ExcelGenerator(headers: any[], rows: any[]): Promise<boolean> {
     const workbook = new Excel.Workbook();
     const workSheets = getWorkSheets(headers.find(x => x.name === 'Attendance').headers)
+    const regex = /^\d{4}-\d{2}-\d{2}$/
     let sheet: any = {};
 
     Object.keys(workSheets).map((workSheet) => {
@@ -58,7 +59,28 @@ export async function ExcelGenerator(headers: any[], rows: any[]): Promise<boole
             })
         })
 
-        rows.map(row => { sheet.addRow(row) })
+        rows.map(row => sheet.addRow(row))
+
+        const headerRow = sheet.getRow(2);
+        headerRow.eachCell((headerCell: any, colIndex: number) => {
+            const columnHeader = headerCell.value;
+            if (regex.test(columnHeader as string)) {
+                sheet.eachRow((row: any) => {
+                    const cell = row.getCell(colIndex);
+                    cell.dataValidation = dataValidation;
+                });
+            }
+        });
+
+        sheet.eachRow({ includeEmpty: true }, (row: any) => {
+            row.eachCell({ includeEmpty: true }, (cell: any) => {
+                if (regex.test(cell._column._key) && cell._row._number > 2) {
+                    cell.protection = { locked: false };
+                }
+            });
+        });
+
+        sheet.protect('#saudigitus_SEMIS_app', lock);
     })
 
     const buf = await workbook.xlsx.writeBuffer()
