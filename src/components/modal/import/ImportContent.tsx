eb68ee3bc8409b67be689e06cca0e22e094b8fc3
cnandza <clayd.nandza@saudigitus.org>
2024-09-19 10:03:49 +0200

    refactor: Update bulk import dropzone component
    Change the dropzone to fit with mockup

--- src/components/modal/import/ImportContent.tsx
+++ src/components/modal/import/ImportContent.tsx
@@ -9,6 +9,7 @@ import ModalComponent from "../Modal";
 import ModalSummaryContent from "./ModalSummaryContent";
 import { getSheetData } from "../../../utils/bulkImport/generateEvents";
 import { getSelectedKey } from "../../../utils/commons/dataStore/getSelectedKey";
+import DropZone from "../../dropzone/DropZone";
 
 const useStyles = makeStyles(() => createStyles({
   previewChip: {
@@ -55,34 +56,38 @@ function ImportContent(props: ImportContentProps): React.ReactElement {
   return (
     <>
       {!openSummaryModal ?
-        <MuiThemeProvider theme={theme}>
-          <DropzoneDialog
-            dialogTitle={"Bulk Attendance"}
-            submitButtonText={"Start Import"}
-            dropzoneText={"Drag and drop a file here or Browse"}
-            Icon={CloudUpload as any}
-            filesLimit={1}
-            showPreviews={false}
-            showPreviewsInDropzone={true}
-            previewGridProps={{
-              container: {
-                spacing: 1,
-                direction: 'row'
-              }
-            }}
-            previewChipProps={{ classes: { root: classes.previewChip } }}
-            previewText="Selected file:"
-            showFileNames={true}
-            showFileNamesInPreview={true}
-            acceptedFiles={[".xlsx"]}
-            open={open}
-            onClose={() => {
-              setOpen(false)
-            }}
-            onSave={(file: any) => handleFileChange(file)}
-            clearOnUnmount={true}
-          />
-        </MuiThemeProvider>
+        <>
+          {/* <MuiThemeProvider theme={theme}>
+            <DropzoneDialog
+              dialogTitle={"Bulk Attendance"}
+              submitButtonText={"Start Import"}
+              dropzoneText={"Drag and drop a file here or Browse"}
+              Icon={CloudUpload as any}
+              filesLimit={1}
+              showPreviews={false}
+              showPreviewsInDropzone={true}
+              previewGridProps={{
+                container: {
+                  spacing: 1,
+                  direction: 'row'
+                }
+              }}
+              previewChipProps={{ classes: { root: classes.previewChip } }}
+              previewText="Selected file:"
+              showFileNames={true}
+              showFileNamesInPreview={true}
+              acceptedFiles={[".xlsx"]}
+              open={open}
+              onClose={() => {
+                setOpen(false)
+              }}
+              onSave={(file: any) => handleFileChange(file)}
+              clearOnUnmount={true}
+            />
+          </MuiThemeProvider> */}
+          <ModalComponent title="Bulk Attendance" children={<DropZone onSave={handleFileChange}/>} open={open} setOpen={setOpen}/>
+
+        </>
         : <ModalComponent title={`Bulk attendance summary`} open={openSummaryModal} setOpen={setOpenSummaryModal}>
           <ModalSummaryContent
             setOpen={setOpenSummaryModal}
